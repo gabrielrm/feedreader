935f79304d42d11280b057931d39c9681e069d8a
Gabriel Radislav Mitin <gabriel.mitin@gmail.com>
2018-05-12 20:51:47 +0300

    Add test suite for menu

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -53,7 +53,10 @@ $(function() {
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* This test suite is about menu element
+     * state and how that is change on click
+     */
+    describe("The menu", function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -82,4 +85,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });
 }());
